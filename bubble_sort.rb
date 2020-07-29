2a558c8ee4b5d7b093657d237a97ac577b3ef382
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-07-29 11:25:39 +0300

    Change return value error on the bubble_sort method fixed, test values added

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -9,21 +9,8 @@ def bubble_sort(array)
       swap(array, index) && sorted = false if compare(array, index) == 1
     end
   end
+  array
 end
-
-def compare(array, index)
-  array[index] <=> array[index + 1]
-end
-
-def swap(array, index)
-  array[index], array[index + 1] = array[index + 1], array[index]
-end
-
-number_array = [99, 5, 9, 6, 2, 3, 1, 12, 44, -4, 55, 3]
-
-bubble_sort(number_array)
-
-p number_array
 
 def bubble_sort_by(array)
   return if array.size <= 1
@@ -35,8 +22,23 @@ def bubble_sort_by(array)
       swap(array, index) && sorted = false if yield(array[index], array[index + 1]).positive?
     end
   end
-  p array
+  array
 end
-bubble_sort_by(%w[hi hello hey]) do |left, right|
+
+def compare(array, index)
+  array[index] <=> array[index + 1]
+end
+
+def swap(array, index)
+  array[index], array[index + 1] = array[index + 1], array[index]
+end
+
+number_array = [99, 5, 9, 6, 2, 3, 1, 12, 44, -4, 55, 3]
+number_array2 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+p bubble_sort(number_array)
+p bubble_sort(number_array2)
+
+sorted_string = bubble_sort_by(%w[hi longstring hello hey there a quick brown fox]) do |left, right|
   left.length - right.length
 end
+p sorted_string
