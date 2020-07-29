b6030d3525b262137e6742906d56ac056b1a8733
Adan Fernandez <adafer123.angra@gmail.com>
2020-07-28 21:12:01 -0500

    Added bubble_sort_by to .rb file and modified linters.yml file

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -26,3 +26,18 @@ number_array = [99, 5, 9, 6, 2, 3, 1, 12, 44, -4, 55, 3]
 bubble_sort(number_array)
 
 p number_array
+
+def bubble_sort_by(array)
+  return if array.size <= 1
+  sorted = false
+  while sorted == false
+    sorted = true
+    (array.length - 1).times do |index|
+      swap(array, index) && sorted = false if yield(array[index], array[index + 1]).positive?
+    end
+  end
+  p array
+end
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
